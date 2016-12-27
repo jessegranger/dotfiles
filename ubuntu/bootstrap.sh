ca4a3d3ba7d80fcc7cee975ee8487c5de92dd631
Jesse Granger <jesse_dailey@fastmail.com>
2016-12-27 07:02:41 -0800

    ubuntu: put everything in the Makefile, remove bootstrap script

--- ubuntu/bootstrap.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install build-essential libboost-all-dev libncurses5-dev ncurses-dev zlib1g-dev \
-	make cmake git autoconf automake protobuf-compiler libutempter-dev
