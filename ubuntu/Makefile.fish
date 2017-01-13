47f148f012eef022fed62fe55fa8e265f8322024
Jesse Granger <jesse_dailey@fastmail.com>
2017-01-13 10:40:55 -0800

    ubuntu: many improvements to build patterns

--- ubuntu/Makefile.fish
+++ ubuntu/Makefile.fish
@@ -3,31 +3,38 @@
 
 FISH_VERSION?=master
 
+PACKAGES+=ncurses-dev libncurses5-dev gettext autoconf
+
 fish: /usr/local/bin/fish
 
 /usr/local/bin/fish: /usr/local/stow/fish-${FISH_VERSION}/bin/fish
-	cd /usr/local/stow && for i in fish-*; do sudo stow -D $$i; done
-	cd /usr/local/stow && sudo stow fish-${FISH_VERSION}
-	grep -q /usr/local/bin/fish /etc/shells || (sudo bash -c "echo /usr/local/bin/fish >> /etc/shells")
+	# [fish] stowing...
+	@cd /usr/local/stow && for i in fish-*; do sudo stow -D $$i; done
+	@cd /usr/local/stow && sudo stow fish-${FISH_VERSION}
+	# [fish] adding to /etc/shells
+	@grep -q /usr/local/bin/fish /etc/shells || (sudo bash -c "echo /usr/local/bin/fish >> /etc/shells")
 
 /usr/local/stow/fish-${FISH_VERSION}/bin/fish: /src/fish/fish
-	cd /src/fish && sudo make -j 2 install
-	sudo touch $@
+	# [fish] installing...
+	@cd /src/fish && sudo make -j 2 install > /dev/null
+	@sudo touch $@
 
 /src/fish/fish: /src/fish/config.log
-	cd /src/fish && make -j 2
+	# [fish] building...
+	@cd /src/fish && make -j 2 > /dev/null
 
 /src/fish/config.log: /src/fish/configure
-	cd /src/fish && ./configure --prefix=/usr/local/stow/fish-${FISH_VERSION} --without-gettext
+	# [fish] configuring...
+	@cd /src/fish && ./configure --prefix=/usr/local/stow/fish-${FISH_VERSION} --without-gettext > /dev/null
 
-/src/fish/configure: /src/fish/.git/HEAD /usr/bin/autoreconf
-	cd /src/fish && autoreconf --no-recursive
+/src/fish/configure: /src/fish/.git/HEAD
+	# [fish] creating configure...
+	@cd /src/fish && autoreconf --no-recursive > /dev/null
+	@touch $@
 
-/usr/bin/autoreconf:
-	sudo apt install autoconf
-
-/src/fish/.git/HEAD:
-	mkdir -p /src
-	cd /src && git clone https://github.com/fish-shell/fish-shell.git fish
-	cd /src/fish && git checkout ${FISH_VERSION}
-	
+/src/fish/.git/HEAD: Makefile.fish
+	# [fish] building...
+	@mkdir -p /src
+	@cd /src && [ -d "fish" ] || git clone https://github.com/fish-shell/fish-shell.git fish
+	@cd /src/fish && git checkout ${FISH_VERSION} && git pull > /dev/null
+	@touch $@
--- ubuntu/Makefile.vim
+++ ubuntu/Makefile.vim
@@ -3,24 +3,43 @@
 
 VIM_VERSION?=master
 
+PACKAGES+=libncurses5-dev libgnome2-dev libgnomeui-dev \
+	libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+	libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
+	python3-dev ruby-dev lua5.1 liblua5.1-dev libperl-dev git
+
 /usr/local/bin/vim: /usr/local/stow/vim-${VIM_VERSION}/bin/vim
-	cd /usr/local/stow && for i in vim-*; do sudo stow -D $$i; done
-	cd /usr/local/stow && sudo stow vim-${VIM_VERSION}
+	# [vim] stowing version '${VIM_VERSION}'...
+	@cd /usr/local/stow && for i in vim-*; do sudo stow -D $$i; done
+	@cd /usr/local/stow && sudo stow vim-${VIM_VERSION}
 	@sudo update-alternatives --install /usr/bin/vi vi /usr/local/bin/vim 100
 
 /usr/local/stow/vim-${VIM_VERSION}/bin/vim: /src/vim/src/vim
-	cd /src/vim && sudo make -j 2 install
-	sudo touch $@
+	# [vim] installing...
+	@cd /src/vim && sudo make -j 2 install > /dev/null
+	@sudo touch $@
 
 vim: /usr/local/bin/vim
 
 /src/vim/src/vim: /src/vim/src/auto/config.cache
-	cd /src/vim && make -j 2
+	# [vim] building...
+	@cd /src/vim && make -j > /dev/null
 
 /src/vim/src/auto/config.cache: /src/vim/.git/HEAD
-	cd /src/vim && ./configure --prefix=/usr/local/stow/vim-${VIM_VERSION} --enable-pythoninterp --enable-rubyinterp --enable-python3interp --with-features=huge
+	# [vim] configuring...
+	@cd /src/vim && ./configure --prefix=/usr/local/stow/vim-${VIM_VERSION} \
+		--with-features=huge \
+		--enable-multibyte \
+		--enable-rubyinterp=yes \
+		--enable-pythoninterp=yes \
+		--enable-perlinterp=yes \
+		--enable-luainterp=yes \
+		--enable-gui=gtk2 \
+		--enable-cscope > /dev/null
 
-/src/vim/.git/HEAD:
-	mkdir -p /src
-	cd /src && git clone https://github.com/vim/vim.git
-	cd /src/vim && git checkout ${VIM_VERSION}
+/src/vim/.git/HEAD: Makefile.vim
+	# [vim] pulling source from github...
+	@mkdir -p /src
+	@cd /src && [ -d "vim" ] || git clone https://github.com/vim/vim.git
+	@cd /src/vim && git checkout ${VIM_VERSION} && git pull
+	@touch $@
