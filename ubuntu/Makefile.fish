173caf2bfe0594b8d2339b0153c7baca47fd4db1
Jesse Granger <jesse_granger@fastmail.com>
2020-07-31 11:29:54 -0700

    more recent fish source builds differently

--- ubuntu/Makefile.fish
+++ ubuntu/Makefile.fish
@@ -1,40 +1,34 @@
 
 # vim: ft=make
 
-FISH_VERSION?=master
+FISH_VERSION?=3.1.2
 
-PACKAGES+=ncurses-dev libncurses5-dev gettext autoconf
+PACKAGES+=ncurses-dev libncurses5-dev gettext
+
+PREFIX=/usr/local/stow/fish-${FISH_VERSION}
+MAKE=env PREFIX='${PREFIX}' make -f GNUmakefile
 
 fish: /usr/local/bin/fish
 
-/usr/local/bin/fish: /usr/local/stow/fish-${FISH_VERSION}/bin/fish
+/usr/local/bin/fish: ${PREFIX}/bin/fish
 	# [fish] stowing...
 	@cd /usr/local/stow && for i in fish-*; do sudo stow -D $$i; done
 	@cd /usr/local/stow && sudo stow fish-${FISH_VERSION}
 	# [fish] adding to /etc/shells
 	@grep -q /usr/local/bin/fish /etc/shells || (sudo bash -c "echo /usr/local/bin/fish >> /etc/shells")
 
-/usr/local/stow/fish-${FISH_VERSION}/bin/fish: /src/fish/fish
+${PREFIX}/bin/fish: /src/fish/build/fish
 	# [fish] installing...
-	@cd /src/fish && sudo make -j 2 install > /dev/null
+	@cd /src/fish && sudo ${MAKE} install > /dev/null
 	@sudo touch $@
 
-/src/fish/fish: /src/fish/config.log
+/src/fish/build/fish: /src/fish/.git/HEAD
 	# [fish] building...
-	@cd /src/fish && make -j 2 > /dev/null
-
-/src/fish/config.log: /src/fish/configure
-	# [fish] configuring...
-	@cd /src/fish && ./configure --prefix=/usr/local/stow/fish-${FISH_VERSION} --without-gettext > /dev/null
-
-/src/fish/configure: /src/fish/.git/HEAD
-	# [fish] creating configure...
-	@cd /src/fish && autoreconf --no-recursive > /dev/null
-	@touch $@
+	@cd /src/fish && ${MAKE} > /dev/null
 
 /src/fish/.git/HEAD: Makefile.fish
-	# [fish] building...
+	# [fish] getting latest code...
 	@mkdir -p /src
 	@cd /src && [ -d "fish" ] || git clone https://github.com/fish-shell/fish-shell.git fish
-	@cd /src/fish && git checkout ${FISH_VERSION} && git pull > /dev/null
+	@cd /src/fish && git fetch && git checkout ${FISH_VERSION} > /dev/null
 	@touch $@
